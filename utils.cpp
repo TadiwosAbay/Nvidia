87c84eedfe23cd2a666733fb28a8a98c200ccb18
TadiwosAbay <68787774+TadiwosAbay@users.noreply.github.com>
2024-07-25 14:05:49 +0100

    Create utils.cpp

---
+++ utils.cpp
@@ -0,0 +1,17 @@
+/* Print the elements of the m x n matrix A. The elements are assumed to be
+   stored by columns if `bycols` is `true` and by rows if `bycols` is false. */
+template <typename float_type>
+void print_matrix(const std::vector<float_type>& A,
+                  const size_t rows,
+                  const size_t cols,
+                  const bool bycols) {
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+        std::cout << std::setw(6);
+        auto next_element = bycols ? A[j*cols+i] : A[i*rows+j];
+        std::cout << __half2float(next_element) << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
