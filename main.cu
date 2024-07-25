8e40af3417ad2381d81caf0792237caba21a39cb
TadiwosAbay <68787774+TadiwosAbay@users.noreply.github.com>
2024-07-25 15:09:56 +0100

    Update main.cu

--- main.cu
+++ main.cu
@@ -41,6 +41,41 @@ int main(){
     //print_matrix(mw.C, M, N, true);
     mw.run_mfma_kernel();
     //print_matrix(mw.C, M, N, true);
+//////////////////////////////////////////////////////////////////////////////////////////////////
+    const binary16_t a11 = __float2half(8);
+    const binary16_t b11 = __float2half(ldexpf(1.0f, -15));
+
+    //const binary16_t b11 = __float2half(1.0f/(1<<15));
+
+    //const binary16_t a21=static_cast<binary16_t>(1.0);
+    //const binary16_t b21=static_cast<binary16_t>(1.0);
+    //float c21_float = -1.0f * (1.0f / (1 << 11));
+    //binary16_t c21 = __float2half(c21_float);
+
+    // Display the values (Note: __half cannot be directly printed)
+  //std::cout << "a11: " << __half2float(a11) << std::endl;
+  //const float16_t c21=static_cast<float16_t>(-1*(1/(1<<2)));
+
+    //std::cout << "c21 without any change: " << __half2float(c21) << std::endl;
+  //std::cout << "c21: " << __half2float(c21) << std::endl;
+    mw.reset_host_matrices();
+
+    mw.A[0]=a11;
+    mw.B[0]=b11;
+    //mw.C[0]=c21;
+    mw.run_mfma_kernel();
+    print_matrix(mw.C, M, N, true);
+
+
+    float expected2= a11*b11;
+    std::cout<<"Expected Result: "<<static_cast<float>(expected2)<<std::endl;
+
+////////////////////////////////////////////
+
+
+
+
+
 
 
     const binary16_t a21 = __float2half(1.0f);
