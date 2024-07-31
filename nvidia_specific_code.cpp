ee237c3c1d50c29a63ac2ba96a52730a7111930f
TadiwosAbay <68787774+TadiwosAbay@users.noreply.github.com>
2024-07-31 15:05:41 +0100

    Update nvidia_specific_code.cpp

--- nvidia_specific_code.cpp
+++ nvidia_specific_code.cpp
@@ -8,28 +8,28 @@ using bfloat16_t = nv_bfloat16;
 
 /* Compute C += A*B, where A, B, and C are 16x16x16 matrices.
    The matrix C is initialized to 0 when `init` is true. */
-template <typename input_t, typename return_t>
-__global__ void wmma_ker(input_t *A, input_t *B, return_t *C, bool init = false) {
+//template <typename input_t, typename return_t>
+//__global__ void wmma_ker(input_t *A, input_t *B, return_t *C, bool init = false) {
 
     // Declare fragments.
-    wmma::fragment<wmma::matrix_a, 16, 16, 16, input_t, wmma::row_major> A_fragment;
-    wmma::fragment<wmma::matrix_b, 16, 16, 16, input_t, wmma::col_major> B_fragment;
-    wmma::fragment<wmma::accumulator, 16, 16, 16, return_t> C_fragment;
+  //  wmma::fragment<wmma::matrix_a, 16, 16, 16, input_t, wmma::row_major> A_fragment;
+  //  wmma::fragment<wmma::matrix_b, 16, 16, 16, input_t, wmma::col_major> B_fragment;
+   // wmma::fragment<wmma::accumulator, 16, 16, 16, return_t> C_fragment;
 
     // Load input matrices and initialize output (if required).
-    wmma::load_matrix_sync(A_fragment, A, 16);
-    wmma::load_matrix_sync(B_fragment, B, 16);
-    if (init)
-        wmma::fill_fragment(C_fragment, 0.0f);
-    else
-        wmma::load_matrix_sync(C_fragment, C, 16, wmma::mem_col_major);
+  //  wmma::load_matrix_sync(A_fragment, A, 16);
+  //  wmma::load_matrix_sync(B_fragment, B, 16);
+  //  if (init)
+    //    wmma::fill_fragment(C_fragment, 0.0f);
+   // else
+     //   wmma::load_matrix_sync(C_fragment, C, 16, wmma::mem_col_major);
 
     // Multiply
-    wmma::mma_sync(C_fragment, A_fragment, B_fragment, C_fragment);
+    //wmma::mma_sync(C_fragment, A_fragment, B_fragment, C_fragment);
 
     // Store the output
-    wmma::store_matrix_sync(C, C_fragment, 16, wmma::mem_col_major);
-}
+    //wmma::store_matrix_sync(C, C_fragment, 16, wmma::mem_col_major);
+//}
 
 
 
