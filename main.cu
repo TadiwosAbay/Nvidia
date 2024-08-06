dc077c78a1bc42d3a30b8e2ff26a2a9a8959b149
TadiwosAbay <68787774+TadiwosAbay@users.noreply.github.com>
2024-08-06 15:51:28 +0100

    Update main.cu

--- main.cu
+++ main.cu
@@ -78,54 +78,54 @@ int main(){
 
 
 
-    printf("subnormal output\n");
-    mw.reset_host_matrices();
-    const float normal_input=ldexpf(-1.0f, -120);
-    const float normal_input2=ldexpf(-1.0f, -7);
-    mw.A[0]=normal_input;
-    mw.B[0]=normal_input2;
-    mw.run_mfma_kernel();
-    print_matrix(mw.C, M, N, true);
+    //printf("subnormal output\n");
+    //mw.reset_host_matrices();
+    //const float normal_input=ldexpf(-1.0f, -120);
+    //const float normal_input2=ldexpf(-1.0f, -7);
+    //mw.A[0]=normal_input;
+    //mw.B[0]=normal_input2;
+    //mw.run_mfma_kernel();
+    //print_matrix(mw.C, M, N, true);
 
 
-    mw.reset_host_matrices();
-    printf("subnormal input\n");
-    const float input_normal=static_cast<float>(8);
-    const float subnormal_input=ldexpf(1.0f, -127);
-    mw.A[0]=input_normal;
-    mw.B[0]=subnormal_input;
-    mw.run_mfma_kernel();
-    print_matrix(mw.C, M, N, true);
+    //mw.reset_host_matrices();
+    //printf("subnormal input\n");
+    //const float input_normal=static_cast<float>(8);
+    //const float subnormal_input=ldexpf(1.0f, -127);
+    //mw.A[0]=input_normal;
+    //mw.B[0]=subnormal_input;
+    //mw.run_mfma_kernel();
+    //print_matrix(mw.C, M, N, true);
 
 
-    mw.reset_host_matrices();
-    printf("Extra bit---20nd bit is the extra?\n");
-    const float one=ldexpf(1.0f, 0);
-    float extra_bit = ldexpf(-1.0f, -20);
+    //mw.reset_host_matrices();
+    //printf("Extra bit---20nd bit is the extra?\n");
+    //const float one=ldexpf(1.0f, 0);
+    //float extra_bit = ldexpf(-1.0f, -20);
     
-    mw.A[0]=one;
-    mw.B[0]=one;
-    mw.C[0]=extra_bit;
-    mw.run_mfma_kernel();
-    print_matrix(mw.C, M, N, true);
+    //mw.A[0]=one;
+    //mw.B[0]=one;
+    //mw.C[0]=extra_bit;
+    //mw.run_mfma_kernel();
+    //print_matrix(mw.C, M, N, true);
 
-    printf("Rounding Mode\n");
-    const float half_ulp =ldexpf(1.0f, -20);
-    mw.reset_host_matrices();
+    //printf("Rounding Mode\n");
+    //const float half_ulp =ldexpf(1.0f, -20);
+    //mw.reset_host_matrices();
     
     
-    mw.A[0]=one;
-    mw.A[1]=one;
-    mw.A[2]=one;
-    mw.A[3]=one;
-    mw.B[0]=one;
-    mw.B[4]=half_ulp;
-    mw.B[8]=half_ulp;
-    mw.B[12]=half_ulp;
+    //mw.A[0]=one;
+    //mw.A[1]=one;
+    //mw.A[2]=one;
+    //mw.A[3]=one;
+    //mw.B[0]=one;
+    //mw.B[4]=half_ulp;
+    //mw.B[8]=half_ulp;
+    //mw.B[12]=half_ulp;
     
-    mw.run_mfma_kernel();
+    //mw.run_mfma_kernel();
    
-    print_matrix(mw.C, M, N, true);
+    //print_matrix(mw.C, M, N, true);
 
 
 
