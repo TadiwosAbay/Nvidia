96ed9f8778b66c44a16cd431483ebcb5f09aa3f7
TadiwosAbay <68787774+TadiwosAbay@users.noreply.github.com>
2024-08-07 17:55:09 +0100

    Update test.cu

--- test.cu
+++ test.cu
@@ -15,13 +15,13 @@ void run_tests(){
     constexpr size_t N = 4;
     constexpr size_t K = 4;
     auto mw = MFMAWrapper<typename InputFormat::type, typename OutputFormat::type>(M, N, K);
-    //auto mw = MFMAWrapper<typename FloatFormat::type, float>(M, N, K);
+    //auto mw = MFMAWrapper<typename InputFormat::type, float>(M, N, K);
 
     // Test 1: Normal input
     mw.reset_host_matrices();
-    const auto normal_input = FloatFormat::four();
-    const auto normal_input2 = FloatFormat::minNormal();
-    mw.A[0] = FloatFormat::one() / normal_input;
+    const auto normal_input = InputFormat::four();
+    const auto normal_input2 = InputFormat::minNormal();
+    mw.A[0] = InputFormat::one() / normal_input;
     mw.B[0] = normal_input2;
     mw.run_mfma_kernel();
     print_matrix(mw.C, M, N, true);
@@ -29,8 +29,8 @@ void run_tests(){
     // Test 2: Subnormal input
     mw.reset_host_matrices();
     std::cout << "Subnormal input\n";
-    const auto input_normal = FloatFormat::four();
-    const auto subnormal_input = FloatFormat::largestSubnormal();
+    const auto input_normal = InputFormat::four();
+    const auto subnormal_input = InputFormat::largestSubnormal();
     mw.A[0] = input_normal;
     mw.B[0] = subnormal_input;
     mw.run_mfma_kernel();
@@ -39,8 +39,8 @@ void run_tests(){
     // Test 3: Extra bit
     mw.reset_host_matrices();
     std::cout << "Extra bit---20th bit is the extra?\n";
-    const auto one = FloatFormat::one();
-    auto extra_bit = FloatFormat::minSubnormal();
+    const auto one = InputFormat::one();
+    auto extra_bit = InputFormat::minSubnormal();
     mw.A[0] = one;
     mw.B[0] = one;
     mw.C[0] = extra_bit;
