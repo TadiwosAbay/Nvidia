c7b02cb5cfb22bc6e478703ad8ee59f3ac66b3d2
TadiwosAbay <68787774+TadiwosAbay@users.noreply.github.com>
2024-08-08 21:17:01 +0100

    Update nvidia_specific_code.cpp

--- nvidia_specific_code.cpp
+++ nvidia_specific_code.cpp
@@ -1,6 +1,7 @@
 #include <mma.h>
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
+#include "fp_utils.h"
 
 using namespace nvcuda;
 
@@ -34,31 +35,85 @@ using bfloat16_t = nv_bfloat16;
     // Destroy cuBLAS handle
     //cublasDestroy(handle);
 //}
-template <typename input_t, typename return_t>
-__global__ void wmma_ker(input_t *A, input_t *B, return_t *C, bool init = false) {
+//template <typename input_t, typename return_t>
+//__global__ void wmma_ker(input_t *A, input_t *B, return_t *C, bool init = false) {
 
     // Declare fragments.
-    wmma::fragment<wmma::matrix_a, 16, 16, 16, input_t, wmma::row_major> A_fragment;
-    wmma::fragment<wmma::matrix_b, 16, 16, 16, input_t, wmma::col_major> B_fragment;
-    wmma::fragment<wmma::accumulator, 16, 16, 16, return_t> C_fragment;
+  //  wmma::fragment<wmma::matrix_a, 16, 16, 16, input_t, wmma::row_major> A_fragment;
+   // wmma::fragment<wmma::matrix_b, 16, 16, 16, input_t, wmma::col_major> B_fragment;
+  //  wmma::fragment<wmma::accumulator, 16, 16, 16, return_t> C_fragment;
 
     // Load input matrices and initialize output (if required).
-    wmma::load_matrix_sync(A_fragment, A, 16);
-    wmma::load_matrix_sync(B_fragment, B, 16);
-    if (init)
-        wmma::fill_fragment(C_fragment, 0.0f);
-    else
-        wmma::load_matrix_sync(C_fragment, C, 16, wmma::mem_col_major);
+  //  wmma::load_matrix_sync(A_fragment, A, 16);
+    //wmma::load_matrix_sync(B_fragment, B, 16);
+    //if (init)
+      //  wmma::fill_fragment(C_fragment, 0.0f);
+    //else
+      //  wmma::load_matrix_sync(C_fragment, C, 16, wmma::mem_col_major);
 
     // Multiply
-    wmma::mma_sync(C_fragment, A_fragment, B_fragment, C_fragment);
+    //wmma::mma_sync(C_fragment, A_fragment, B_fragment, C_fragment);
 
     // Store the output
-    wmma::store_matrix_sync(C, C_fragment, 16, wmma::mem_col_major);
+    //wmma::store_matrix_sync(C, C_fragment, 16, wmma::mem_col_major);
+//}
+
+template <typename input_t, typename return_t>
+__global__ void wmma_ker(input_t *A, input_t *B, return_t *C, bool init = false) {
+    if constexpr (std::is_same_v<input_t, binary16_t>) {
+        // For binary16_t (which is similar to __half)
+        wmma::fragment<wmma::matrix_a, 16, 16, 16, binary16_t, wmma::row_major> A_fragment;
+        wmma::fragment<wmma::matrix_b, 16, 16, 16, binary16_t, wmma::col_major> B_fragment;
+        wmma::fragment<wmma::accumulator, 16, 16, 16, return_t> C_fragment;
+
+        wmma::load_matrix_sync(A_fragment, A, 16);
+        wmma::load_matrix_sync(B_fragment, B, 16);
+        if (init)
+            wmma::fill_fragment(C_fragment, 0.0f);
+        else
+            wmma::load_matrix_sync(C_fragment, C, 16, wmma::mem_col_major);
+
+        wmma::mma_sync(C_fragment, A_fragment, B_fragment, C_fragment);
+        wmma::store_matrix_sync(C, C_fragment, 16, wmma::mem_col_major);
+    } else if constexpr (std::is_same_v<input_t, binary32_t>) {
+        // For binary32_t (which maps to float, using tf32)
+        wmma::fragment<wmma::matrix_a, 16, 16, 8, wmma::precision::tf32, wmma::row_major> A_fragment;
+        wmma::fragment<wmma::matrix_b, 16, 16, 8, wmma::precision::tf32, wmma::col_major> B_fragment;
+        wmma::fragment<wmma::accumulator, 16, 16, 8, return_t> C_fragment;
+
+        for (int iter = 0; iter < 2; iter++) {
+            wmma::load_matrix_sync(A_fragment, A + 8 * iter, 16);
+            wmma::load_matrix_sync(B_fragment, B + 8 * iter, 16);
+            if (init)
+                wmma::fill_fragment(C_fragment, 0.0f);
+            else
+                wmma::load_matrix_sync(C_fragment, C, 16, wmma::mem_col_major);
+
+            wmma::mma_sync(C_fragment, A_fragment, B_fragment, C_fragment);
+            wmma::store_matrix_sync(C, C_fragment, 16, wmma::mem_col_major);
+        }
+    } else if constexpr (std::is_same_v<input_t, binary64_t>) {
+        // For binary64_t (which maps to double)
+        wmma::fragment<wmma::matrix_a, 8, 8, 4, double, wmma::row_major> A_fragment;
+        wmma::fragment<wmma::matrix_b, 8, 8, 4, double, wmma::col_major> B_fragment;
+        wmma::fragment<wmma::accumulator, 8, 8, 4, return_t> C_fragment;
+
+        int warpM = threadIdx.x / 32;
+        int warpN = threadIdx.y;
+
+        for (int iter = 0; iter < 4; iter++) {
+            wmma::load_matrix_sync(A_fragment, A + warpN * 16 * 8 + iter * 4, 16);
+            wmma::load_matrix_sync(B_fragment, B + iter * 4 + warpM * 16 * 8, 16);
+            if (init)
+                wmma::fill_fragment(C_fragment, 0.0f);
+            else
+                wmma::load_matrix_sync(C_fragment, C + warpM * 8 + warpN * 16 * 8, 16, wmma::mem_row_major);
+
+            wmma::mma_sync(C_fragment, A_fragment, B_fragment, C_fragment);
+            wmma::store_matrix_sync(C + warpM * 8 + warpN * 16 * 8, C_fragment, 16, wmma::mem_row_major);
+        }
+    }
 }
-
-
-
 // CUDA kernel for matrix multiplication with float (FP32) inputs
 //__global__ void matmul_kernel(const float* A, const float* B, float* C, int M, int N, int K) {
   //  int row = blockIdx.y * blockDim.y + threadIdx.y;
