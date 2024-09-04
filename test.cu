f02cb701928fd2c4bee7385ae6ca208e0d78d1f1
TadiwosAbay <68787774+TadiwosAbay@users.noreply.github.com>
2024-09-04 19:49:18 +0900

    Update test.cu

--- test.cu
+++ test.cu
@@ -34,60 +34,60 @@ void run_tests(){
     //auto mw = MFMAWrapper<typename InputFormat::storageFormat, float>(M, N, K);
 
     // Test 1: Normal input
-    mw.reset_host_matrices();
-    const auto normal_input  = InputFormat::constant(4);
-    const auto normal_input2 = InputFormat::minNormal();
-    //const auto normal_input2 = InputFormat::minimumNormal();
-    mw.A[0] = InputFormat::constant(1) / normal_input;
-    mw.B[0] = normal_input2;
-    mw.run_mfma_kernel();
-    //print_matrix<InputFormat>(mw.A, M, N, true);
-    //print_matrix<InputFormat>(mw.B, M, N, false);
-    print_matrix<InputFormat>(mw.C, M, N, true);
-
-    // Test 2: Subnormal input
-    mw.reset_host_matrices();
-    std::cout << "Subnormal input\n";
-    const auto input_normal = InputFormat::constant(4);
-    const auto subnormal_input = InputFormat::midwaySubnormal();
-    mw.A[0] = input_normal;
-    mw.B[0] = subnormal_input;
-    mw.run_mfma_kernel();
-    //print_matrix<InputFormat>(mw.B, M, N, false);
-    print_matrix<InputFormat>(mw.C, M, N, true);
-
-    std::cout << input_normal << std::endl;
-
-    // Test 3: Extra bit
-    mw.reset_host_matrices();
-    std::cout << "Extra bit---20th bit is the extra?\n";
-    const auto one = InputFormat::constant(1);
-    auto extra_bit = InputFormat::extra_bit();
-    //print_matrix<InputFormat>(mw.A, M, N, true);
-    //print_matrix<InputFormat>(mw.B, M, N, false);
+    //mw.reset_host_matrices();
+    //const auto normal_input  = InputFormat::constant(4);
+    //const auto normal_input2 = InputFormat::minNormal();
+    ////const auto normal_input2 = InputFormat::minimumNormal();
+    //mw.A[0] = InputFormat::constant(1) / normal_input;
+    //mw.B[0] = normal_input2;
+    //mw.run_mfma_kernel();
+    ////print_matrix<InputFormat>(mw.A, M, N, true);
+    ////print_matrix<InputFormat>(mw.B, M, N, false);
     //print_matrix<InputFormat>(mw.C, M, N, true);
-    mw.A[0] = one;
-    mw.B[0] = one;
-    mw.C[0] = static_cast<decltype(extra_bit)>(-1)*extra_bit;
-    mw.run_mfma_kernel();
-    //print_matrix<InputFormat>(mw.A, M, N, true);
-    //print_matrix<InputFormat>(mw.B, M, N, false);
-    print_matrix<InputFormat>(mw.C, M, N, true);
 
-    // Test 4: Rounding Mode
-    std::cout << "Rounding Mode\n";
-    const auto half_ulp = InputFormat::extra_bit(); // This part may need to be generalized based on format
-    mw.reset_host_matrices();
-    mw.A[0] = one;
-    mw.A[1] = one;
-    mw.A[2] = one;
-    mw.A[3] = one;
-    mw.B[0] = one;
-    mw.B[1] = half_ulp;
-    mw.B[2] = half_ulp;
-    mw.B[3] = half_ulp;
-    mw.run_mfma_kernel();
-    print_matrix<InputFormat>(mw.C, M, N, true);
+    //// Test 2: Subnormal input
+    //mw.reset_host_matrices();
+    //std::cout << "Subnormal input\n";
+    //const auto input_normal = InputFormat::constant(4);
+    //const auto subnormal_input = InputFormat::midwaySubnormal();
+    //mw.A[0] = input_normal;
+    //mw.B[0] = subnormal_input;
+    //mw.run_mfma_kernel();
+    ////print_matrix<InputFormat>(mw.B, M, N, false);
+    //print_matrix<InputFormat>(mw.C, M, N, true);
+
+    //std::cout << input_normal << std::endl;
+
+    //// Test 3: Extra bit
+    //mw.reset_host_matrices();
+    //std::cout << "Extra bit---20th bit is the extra?\n";
+    //const auto one = InputFormat::constant(1);
+    //auto extra_bit = InputFormat::extra_bit();
+    ////print_matrix<InputFormat>(mw.A, M, N, true);
+    ////print_matrix<InputFormat>(mw.B, M, N, false);
+    ////print_matrix<InputFormat>(mw.C, M, N, true);
+    //mw.A[0] = one;
+    //mw.B[0] = one;
+    //mw.C[0] = static_cast<decltype(extra_bit)>(-1)*extra_bit;
+    //mw.run_mfma_kernel();
+    ////print_matrix<InputFormat>(mw.A, M, N, true);
+    ////print_matrix<InputFormat>(mw.B, M, N, false);
+    //print_matrix<InputFormat>(mw.C, M, N, true);
+
+    //// Test 4: Rounding Mode
+    //std::cout << "Rounding Mode\n";
+    //const auto half_ulp = InputFormat::extra_bit(); // This part may need to be generalized based on format
+    //mw.reset_host_matrices();
+    //mw.A[0] = one;
+    //mw.A[1] = one;
+    //mw.A[2] = one;
+    //mw.A[3] = one;
+    //mw.B[0] = one;
+    //mw.B[1] = half_ulp;
+    //mw.B[2] = half_ulp;
+    //mw.B[3] = half_ulp;
+    //mw.run_mfma_kernel();
+    //print_matrix<InputFormat>(mw.C, M, N, true);
 
     mw.run_test();
 
