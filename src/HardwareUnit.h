15e103f2a275c8d260be723105eabcb8d8fd0213
TadiwosAbay <68787774+TadiwosAbay@users.noreply.github.com>
2024-11-22 19:53:15 +0900

    Update HardwareUnit.h

--- src/HardwareUnit.h
+++ src/HardwareUnit.h
@@ -201,7 +201,69 @@ class HardwareUnit {
             return result;
         }
 
+        bool check_extra_bits_with_three_bits() {
+            reset_host_matrices();
+            auto ulp = InputFormat::machineEpsilon();
+            auto ulp_2 = InputFormat::machineEpsilon() * InputFormat::constant(2);
+            auto ulp_4 = InputFormat::constant(4) * InputFormat::machineEpsilon();
 
+            auto last_3_bits = ulp + ulp_2 + ulp_4;
+
+            // Test Case 1
+            A[0] = InputFormat::constant(1) + (InputFormat::constant(2^-2));
+            B[0] = InputFormat::constant(1.0);
+            A[1] = InputFormat::constant(1) + (ulp + ulp_2);
+            B[1] = InputFormat::constant(1.0);
+
+            run_mfma_kernel();
+            bool test1_result = (C[0] & last_3_bits);
+
+            // Test Case 2
+            reset_host_matrices();
+            A[0] = InputFormat::constant(1) + (InputFormat::constant(2^-2));
+            B[0] = InputFormat::constant(1.0);
+            A[1] = InputFormat::constant(1) + ulp;
+            B[1] = InputFormat::constant(1.0);
+
+            run_mfma_kernel();
+            bool test2_result = (C[0] & last_3_bits);
+
+            if (last_3_bits == test1_result) {
+                std::cout << "has one extra bit" << std::endl;
+            } else if (last_3_bits == test2_result) {
+                std::cout << "has three extra bits" << std::endl;
+            } else {
+                std::cout << "has two extra bits" << std::endl;
+            }
+
+            // Additional RTN-TE Test
+            reset_host_matrices();
+            A[0] = InputFormat::constant(1) + (InputFormat::constant(2^-2));
+            B[0] = InputFormat::constant(1.0);
+            A[1] = InputFormat::constant(1) + (ulp_4 + ulp);
+            B[1] = InputFormat::constant(1.0);
+
+            run_mfma_kernel();
+            bool test3_result = (C[0] & last_3_bits);
+
+            reset_host_matrices();
+            A[0] = InputFormat::constant(1) + (InputFormat::constant(2^-2));
+            B[0] = InputFormat::constant(1.0);
+            A[1] = InputFormat::constant(1) + (ulp_4 + ulp_2);
+            B[1] = InputFormat::constant(1.0);
+
+            run_mfma_kernel();
+            bool test4_result = (C[0] & last_3_bits);
+
+            if (last_3_bits == test3_result) {
+                std::cout << "has three extra bits" << std::endl;
+            } else if (last_3_bits == test4_result) {
+                std::cout << "has two extra bits" << std::endl;
+            } else {
+                std::cout << "has one extra bit" << std::endl;
+            }
+            return true;
+        }
 
 
 
